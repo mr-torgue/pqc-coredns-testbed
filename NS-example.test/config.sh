83952b4a27dd0a1828fc52ab68a1e9dfe12f32df
mr_torgue <me+github@folmer.info>
2026-08-03 12:02:43 +1000

    fixes in config.sh for the auth ns

--- NS-example.test/config.sh
+++ NS-example.test/config.sh
@@ -2,61 +2,92 @@
 
 DATE_TIME=$(date +"%Y%m%d-%H%M%S")
 
-DOMAIN="example.test."
 TLS_DS="rsa:2048"
-DNSSEC_DS="P256_FALCON512"
-ZONEFILE="db.example.test"
+DNSSEC_DS_LIST=("FALCON512" "P256_FALCON512" "DILITHIUM2" "P256_DILITHIUM" "SPHICS" "P256_SPHINCS" "P256" "NA")
 CONFIG_NAME="config"
 
-while getopts "d:t:a:z:n:" opt; do
-  case $opt in
-    t) TLS_DS="$OPTARG" ;;
-    a) DNSSEC_DS="$OPTARG" ;;
-    z) ZONEFILE="$OPTARG" ;;
-    n) CONFIG_NAME="$OPTARG" ;;
-    *) echo "Usage: $0 [-d <dsset>] [-t <tls_ds>] [-a <dnssec_ds>] [-z <zonefile>] [-n <config_name>]" >&2; exit 1 ;;
-  esac
+while getopts "t:a:l:i:n:" opt; do
+	case $opt in
+		t) TLS_DS="$OPTARG" ;;
+		a) DNSSEC_DS_LIST=("$OPTARG") ;;
+		l) LOC="$OPTARG" ;;
+		i) NS_IP="$OPTARG" ;;
+		n) CONFIG_NAME="$OPTARG" ;;
+		*) echo "Usage: $0 [-d <dsset>] [-t <tls_ds>] [-a <dnssec_ds>] [-l <location>] [-i <ns_ip>] [-n <config_name>]" >&2; exit 1 ;;
+	esac
 done
 
-CONFIG_DIR="${CONFIG_NAME}-${DATE_TIME}"
-mkdir -p "${CONFIG_DIR}"
-
-
-../scripts/genkey.sh -f "${DOMAIN}" -t "${TLS_DS}" -d "${DNSSEC_DS}" 
-../scripts/signzone.sh -z "${ZONEFILE}" -f "${DOMAIN}"
-
-# export DS record for easy import
-DSRR="dsset-example.test."
-if [ ! -f "$DSRR" ]; then
-    echo "Error: File '$file' not found." >&2
-    exit 1
+# check
+if [ -z "$LOC" ] || [ -z "$NS_IP" ]; then
+	echo "Error: Both location and NS IP must be set" >&2
+	exit 1
 fi
 
-checksum=$(sha256sum "$DSRR" | awk '{print $1}')
+CONFIG_DIR="${CONFIG_NAME}-${LOC}-${DATE_TIME}"
+mkdir -p "${CONFIG_DIR}"
 
-echo "cat > $DSRR << 'EOF'"
-cat "$DSRR"
-echo "EOF"
-echo "echo '$checksum  $DSRR' | sha256sum --check"
+DOMAINS=()
+IMPORT_SCRIPT=""
+for DNSSEC_DS in "${DNSSEC_DS_LIST[@]}"; do
+    # generate zone file
+    DOMAIN="${DNSSEC_DS}-${LOC}.test"
+    ZONEFILE="db.${DNSSEC_DS}-${LOC}.test"
+    ../scripts/genzone -a "$DNSSEC_DS" -l "$LOC" -n 1000 -w > $ZONEFILE
+
+    if [ "$DNSSEC_DS" != "NA" ]; then
+      	../scripts/gendnskey.sh -f "${DOMAIN}" -d "${DNSSEC_DS}"
+		../scripts/signzone.sh -z "$ZONEFILE" -f "${DOMAIN}"
+
+		# export DS record for easy import
+		DSRR="dsset-${DOMAIN}."
+		if [ ! -f "$DSRR" ]; then
+			echo "Error: File '$file' not found." >&2
+			exit 1
+		fi
+		checksum=$(sha256sum "$DSRR" | awk '{print $1}')
+
+		# verifies DSSET on .test NS
+		IMPORT_SCRIPT+=$(echo "cat > $DSRR << 'EOF'\n")
+		IMPORT_SCRIPT+=$(cat "$DSRR")
+		IMPORT_SCRIPT+=$(echo "EOF")
+		IMPORT_SCRIPT+=$(echo "echo '$checksum  $DSRR' | sha256sum --check")
+
+		# changes the NS and A record on the .test NS
+		IMPORT_SCRIPT+=$(echo "if grep -q \"ns1.${DOMAIN}.\s*IN\s*NS\" db.test; then\n")
+		IMPORT_SCRIPT+=$(echo "    sed -i \"/ns1.${DOMAIN}.\s*IN\s*A/c ns1.${DOMAIN}.	IN	A	${NS_IP}\" db.test\n")
+		IMPORT_SCRIPT+=$(echo "else\n")
+		IMPORT_SCRIPT+=$(echo "    echo \"ns1.${DOMAIN}.	IN	NS	ns1.${DOMAIN}.\" >> db.test\n")
+		IMPORT_SCRIPT+=$(echo "    echo \"ns1.${DOMAIN}.	IN	A	${NS_IP}\" >> db.test\n")
+		IMPORT_SCRIPT+=$(echo "fi\n")
+
+		# copy dnssec files
+		cp db.${DOMAIN} ${CONFIG_DIR}
+		mv db.${DOMAIN}.signed ${CONFIG_DIR}
+		mv $DSRR ${CONFIG_DIR}
+    fi
+    DOMAINS+=("$DOMAIN")
+done
+
+# generate a TLS certificate
+../scripts/gentlskey.sh -f "${DOMAINS}" -t "${TLS_DS}"
 
 # copy files
 mv Kexample* ${CONFIG_DIR}
 cp CoreFile ${CONFIG_DIR}
-cp db.example.test ${CONFIG_DIR}
-mv db.example.test.signed ${CONFIG_DIR}
-mv $DSRR ${CONFIG_DIR}
 mv key.pem ${CONFIG_DIR}
 mv cert.pem ${CONFIG_DIR}
 
+# print import script
+echo "Import script for .test nameserver:"
+echo "$IMPORT_SCRIPT"
+
 cat > "${CONFIG_DIR}/config.json" <<EOF
 {
-  "domain": "${DOMAIN}",
-  "TLS Signature Scheme": "${TLS_DS}",
-  "DNSSEC Algorithm": "${DNSSEC_DS}",
-  "Config Directory": "${CONFIG_DIR}",
-  "Date": "${DATE_TIME}",
-  "Config Name": "${CONFIG_NAME}"
+	"domains": "${DOMAINS}",
+	"TLS Signature Scheme": "${TLS_DS}",
+	"DNSSEC Algorithm": "${DNSSEC_DS}",
+	"Config Directory": "${CONFIG_DIR}",
+	"Date": "${DATE_TIME}",
+	"Config Name": "${CONFIG_NAME}"
 }
 EOF
-
-echo "Don't forget to change the IP addresses in the db file!!"
